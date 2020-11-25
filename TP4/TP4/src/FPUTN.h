5f245c1fd351170c8cc81cc5d7fcb43f4169e985
ferport <portillofernandobek@gmail.com>
2020-11-25 08:44:17 -0300

    TP4 finalizado

---
+++ TP4/TP4/src/FPUTN.h
@@ -0,0 +1,9 @@
+#ifndef FPUTN_H_INCLUDED
+#define FPUTN_H_INCLUDED
+
+
+
+#endif // FPUTN_H_INCLUDED
+
+int validacionCadena(char* cadena);
+void formatearCadena(char* cadena);
---
+++ TP4/TP4/src/menus.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "menus.h"
+
+int menu(){
+    int opcion;
+
+    system("cls");
+
+    printf("***AMB EMPLEADOS***\n\n");
+    printf("1) Cargar los datos de los empleados desde el archivo data.csv (modo texto).\n");
+    printf("2) Cargar los datos de los empleados desde el archivo data.bin (modo binario).\n");
+    printf("3) Alta de empleado.\n");
+    printf("4) Modificar datos de empleado.\n");
+    printf("5) Baja de empleado.\n");
+    printf("6) Listar empleados.\n");
+    printf("7) Ordenar empleados.\n");
+    printf("8) Revisar horas.\n");
+    printf("9) Listar empleados que trabajan menos de 48HS.\n");
+    printf("10) Guardar los datos de los empleados en el archivo data.csv (modo texto).\n");
+    printf("11) Guardar los datos de los empleados en el archivo data.bin (modo binario).\n");
+    printf("12) Salir.\n");
+    printf("Ingrese una opcion: ");
+    fflush(stdin);
+    scanf("%d", &opcion);
+
+    return opcion;
+}
